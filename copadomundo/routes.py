bf2bb98d3422911ef427c6b29be68bd2781cb7c8
Alssousa <alssousa@proton.me>
2022-09-06 13:11:40 -0300

    Todas as rotas definidas e prontas para serem desenvolvidas.

--- copadomundo/routes.py
+++ copadomundo/routes.py
@@ -21,4 +21,28 @@ def add_usuario():
             database.session.commit()
             return redirect(url_for('home'))
 
+    return render_template('cadastro.html')
 
+@app.route('/usuario/login', methods=['POST'])
+def login():
+    return render_template('login.html')
+
+
+@app.route('/liga/ranking')
+def ranking_usuarios():
+    return render_template('ranking_usuario.html')
+
+
+@app.route('/selecoes/partida/addpartida', methods=['POST'])
+def add_partida():
+    return render_template('add_partida.html')
+
+
+@app.route('/selecoes/partida/todas', methods=['POST', 'GET'])
+def all_partidas():
+    return render_template('all_partidas.html')
+
+
+@app.route('/selecoes/partidas/<id_partida>', methods=['POST'])
+def definir_resultado(id_partida):
+    return render_template('definir_resultado.html')
