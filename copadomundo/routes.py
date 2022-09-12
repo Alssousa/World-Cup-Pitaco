405c44f4df91e4fe56a96c17c6bf31e393c6c474
Alssousa <alssousa@proton.me>
2022-09-12 16:52:20 -0300

    Rota de adicionar partida funcional

--- copadomundo/routes.py
+++ copadomundo/routes.py
@@ -1,6 +1,6 @@
 from copadomundo import app, bcrypt, database
 from flask import request, render_template, redirect, url_for
-from copadomundo.models import Usuario
+from copadomundo.models import Usuario, Partida, Selecao, Palpite
 from copadomundo.form import FormAddPartida
 
 
@@ -35,9 +35,37 @@ def ranking_usuarios():
     return render_template('ranking_usuario.html')
 
 
-@app.route('/selecoes/partida/addpartida', methods=['POST, GET'])
+@app.route('/selecoes/partida/addpartida', methods=['GET', 'POST'])
 def add_partida():
     formaddpartida = FormAddPartida()
+    
+    if formaddpartida.validate_on_submit():
+        selecao_casa = formaddpartida.selecao_casa.data
+        selecao_fora = formaddpartida.selecao_fora.data
+        data_partida = formaddpartida.data_partida.data
+        descricao = f"{selecao_casa} x {selecao_fora}"
+        partida = Partida(descricao=descricao, data_partida=data_partida)
+        
+        if not Partida.query.filter_by(descricao=descricao).filter_by(data_partida=data_partida).first():
+            try:
+                database.session.add(partida)
+                database.session.commit()
+            except Exception as e:
+                print("Erro ao criar a partida: " + str(e))
+                
+            selecao_casa = Selecao.query.filter_by(nome=selecao_casa).first()
+            selecao_fora = Selecao.query.filter_by(nome=selecao_fora).first()
+            
+            if selecao_casa and selecao_fora:
+                partida = Partida.query.filter_by(descricao=descricao).first()
+                print(f"\n{partida}\n")
+                selecao_casa.partidas.append(partida)
+                selecao_fora.partidas.append(partida)       
+                database.session.add(selecao_casa)
+                database.session.commit()
+                print("\n\nPartida vinculada as selecoes")
+        
+        return redirect(url_for('add_partida'))
     
     return render_template('add_partida.html', formaddpartida=formaddpartida)
 
