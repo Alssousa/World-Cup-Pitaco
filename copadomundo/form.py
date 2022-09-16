3455179d69bb773719488df7bc8bf221927b4ce8
Alssousa <alssousa@proton.me>
2022-09-16 15:56:24 -0300

    Msg de erro caso email já existe

--- copadomundo/form.py
+++ copadomundo/form.py
@@ -2,6 +2,7 @@ from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed
 from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField, SelectField, DateField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from copadomundo.models import Usuario
 
 
 class FormAddPartida(FlaskForm):
@@ -10,11 +11,18 @@ class FormAddPartida(FlaskForm):
     data_partida = DateField('Data da Partida', validators=[DataRequired()])
     btn_submit = SubmitField('Adicionar')
 
+
 class FormCadastro(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     email = StringField('Email', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     btn_submit = SubmitField('Cadastrar')
+    
+    def validate_email(self, email):
+        user = Usuario.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('O email já esta em uso. Favor, tente outro email ou faça login para continuar')
+    
     
 class FormLogin(FlaskForm):
     email = StringField('Email', validators=[DataRequired()])
