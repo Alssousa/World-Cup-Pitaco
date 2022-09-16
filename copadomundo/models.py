e10cb46f01a51e79bbe4d3e952b35ecc641ec805
Alssousa <alssousa@proton.me>
2022-09-16 15:56:53 -0300

    controle login

--- copadomundo/models.py
+++ copadomundo/models.py
@@ -1,8 +1,13 @@
-from copadomundo import database
+from copadomundo import database, login_manager
 from datetime import datetime
+from flask_login import UserMixin
 
 
-class Usuario(database.Model):
+@login_manager.user_loader
+def load_usuario(id_usuario):
+    return Usuario.query.get(int(id_usuario))
+
+class Usuario(database.Model, UserMixin):
     id = database.Column(database.Integer, primary_key=True)
     username = database.Column(database.String, nullable=False, unique=True)
     senha = database.Column(database.String, nullable=False)
