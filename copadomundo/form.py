6835d96bfaa1aa68c99d97998cd1bbccba020dd6
Alssousa <alssousa@proton.me>
2022-09-19 17:51:13 -0300

    criado campo de hora no formulario add_partida

--- copadomundo/form.py
+++ copadomundo/form.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed
-from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField, SelectField, DateField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField, SelectField, DateField, TimeField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from copadomundo.models import Usuario
 
@@ -9,6 +9,7 @@ class FormAddPartida(FlaskForm):
     selecao_casa = StringField('Seleção Casa', validators=[DataRequired()])
     selecao_fora = StringField('Seleção Fora', validators=[DataRequired()])
     data_partida = DateField('Data da Partida', validators=[DataRequired()])
+    hora_partida = TimeField('Hora Partida', validators=[DataRequired()])
     btn_submit = SubmitField('Adicionar')
 
 
