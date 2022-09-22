f0ceda0a99a7312fd92d2d6df5279df3dfed76e0
Alssousa <alssousa@proton.me>
2022-09-21 23:31:32 -0300

    rota de definir resultado da partida criado

--- copadomundo/routes.py
+++ copadomundo/routes.py
@@ -1,7 +1,7 @@
 from copadomundo import app, bcrypt, database
 from flask import request, render_template, redirect, url_for, flash, jsonify
 from copadomundo.models import Usuario, Partida, Selecao, Palpite, Grupo
-from copadomundo.form import FormAddPartida, FormCadastro, FormLogin
+from copadomundo.form import FormAddPartida, FormCadastro, FormLogin, FormDefinirResultado
 from flask_login import login_user, logout_user, current_user, login_required
 from collections import OrderedDict
 from datetime import datetime, date, timedelta
@@ -80,9 +80,7 @@ def add_partida():
     formaddpartida = FormAddPartida()
     formaddpartida.selecao_casa.choices = [(selecao.id, selecao.nome) for selecao in Selecao.query.filter_by(id_grupo=1).all()]
     formaddpartida.selecao_fora.choices = [(selecao.id, selecao.nome) for selecao in Selecao.query.filter_by(id_grupo=1).all()]
-    
-    
-    
+       
     if formaddpartida.is_submitted():
         print("\nENTROU AKIIIIIIIIIIIII\n")
         selecao_casa = Selecao.query.get(formaddpartida.selecao_casa.data)
@@ -104,7 +102,9 @@ def add_partida():
                 partida = Partida.query.filter_by(descricao=descricao).first()
                 print(f"\n{partida}\n")
                 selecao_casa.partidas.append(partida)
-                selecao_fora.partidas.append(partida)       
+                selecao_fora.partidas.append(partida)   
+                selecao_casa.qnt_jogos += 1   
+                selecao_fora.qnt_jogos += 1 
                 database.session.add_all([selecao_casa, selecao_fora])
                 database.session.commit()
                 print("\n\nPartida vinculada as selecoes")
@@ -135,7 +135,8 @@ def selecoes(grupo):
 def todas_partidas():
     partidas = Partida.query.order_by(Partida.data_partida).all()
     datas_partidas = []
-    data_atual = datetime.now()
+    data_atual = datetime.now()           
+    
     if partidas:
         for partida in partidas:
             datas_partidas.append(partida.data_partida)
@@ -144,9 +145,55 @@ def todas_partidas():
     return render_template('partidas.html', partidas=partidas, datas_partidas=datas_partidas, data_atual=data_atual)
 
 
-@app.route('/selecoes/partidas/<id_partida>', methods=['POST'])
+@app.route('/selecoes/partida/<id_partida>', methods=['GET', 'POST'])
 def definir_resultado(id_partida):
-    return render_template('definir_resultado.html', )
+    formresultado = FormDefinirResultado()
+    partida = Partida.query.get(id_partida)
+    partida.status = formresultado.status.data
+    partida.gol_casa = formresultado.casa_gol.data
+    partida.gol_fora = formresultado.fora_gol.data
+    selecao_casa = Selecao.query.get(partida.selecoes[0].id)
+    selecao_fora = Selecao.query.get(partida.selecoes[1].id)
+    
+    print(f"selecao casa: {selecao_casa.nome}, selecao fora: {selecao_fora.nome}")
+    if formresultado.validate_on_submit():
+        casa_gol = formresultado.casa_gol.data
+        fora_gol = formresultado.fora_gol.data
+        status = formresultado.status.data
+        
+        selecao_casa.gols_marcado = casa_gol
+        selecao_fora.gols_marcado = fora_gol
+        
+        if casa_gol > fora_gol:
+            selecao_casa.vitorias += 1
+            selecao_casa.pontos += 3
+            selecao_casa.gols_sofrido += fora_gol
+            selecao_fora.derrotas += 1
+            selecao_fora.gols_sofrido += casa_gol
+        elif fora_gol > casa_gol:
+            selecao_fora.vitorias += 1
+            selecao_fora.pontos += 3
+            selecao_fora.gols_sofrido += casa_gol
+            selecao_casa.derrotas += 1
+            selecao_casa.gols_sofrido += fora_gol
+        else:
+            selecao_casa.pontos += 1
+            selecao_fora.pontos += 1
+            selecao_casa.gols_sofrido += fora_gol
+            selecao_fora.gols_sofrido += casa_gol
+            selecao_fora.empates += 1
+            selecao_casa.empates += 1
+                   
+        try:
+            print("akiiii")
+            database.session.add_all([selecao_casa, selecao_fora, partida])
+            database.session.commit()
+            flash('Resultado definido com sucesso', 'success')
+            return redirect(url_for('todas_partidas'))
+        except Exception as e:
+            print("Erro ao definir resultado da partida: " + str(e))
+    
+    return render_template('tela_add_resultado.html', formresultado=formresultado, selecao_casa=selecao_casa, selecao_fora=selecao_fora)
 
 
 @app.route('/usuario/<partida>/palpite', methods=['GET', 'POST'])
@@ -169,6 +216,8 @@ def ranking():
     usuarios = Usuario.query.all()
     
     return render_template('tela_rank.html', usuarios=usuarios)
+
+
 
 
 @app.route('/logout')
