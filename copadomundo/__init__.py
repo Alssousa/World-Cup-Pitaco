d37136555c7a160a7a575a7f0d3ecf966c88e412
Alssousa <alssousa@proton.me>
2022-10-07 15:27:06 -0300

    .

--- copadomundo/__init__.py
+++ copadomundo/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask_session import Session
+#from flask_session import Session
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
@@ -7,9 +7,9 @@ from flask_login import LoginManager
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'C0P4_D0_MUND0_P1T4C0'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///worldcuppitaco.db'
-app.config['SESSION_PERMANET'] = False
-app.config['SESSION_TYPE'] = 'filesystem'
-Session(app)
+#app.config['SESSION_PERMANET'] = False
+#app.config['SESSION_TYPE'] = 'filesystem'
+#Session(app)
 database = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 
