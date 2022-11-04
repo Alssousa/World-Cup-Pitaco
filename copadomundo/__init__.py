b4efddfe7583cb992f28ce09485696dec090c944
Alssousa <alssousa@proton.me>
2022-11-04 16:38:05 -0300

    remoção da lib flask-session

--- copadomundo/__init__.py
+++ copadomundo/__init__.py
@@ -1,5 +1,4 @@
 from flask import Flask
-#from flask_session import Session
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
@@ -7,9 +6,6 @@ from flask_login import LoginManager
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'C0P4_D0_MUND0_P1T4C0'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///worldcuppitaco.db'
-#app.config['SESSION_PERMANET'] = False
-#app.config['SESSION_TYPE'] = 'filesystem'
-#Session(app)
 database = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 
