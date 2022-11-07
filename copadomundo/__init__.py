fdabd21c3933c533ed821d6f23ef3ec16aef1768
Alssousa <alssousa@proton.me>
2022-11-07 15:45:41 -0300

    teste conexão db parte 3

--- copadomundo/__init__.py
+++ copadomundo/__init__.py
@@ -5,7 +5,7 @@ from flask_login import LoginManager
 import os
 import urllib.parse
 
-params = urllib.parse.quote_plus("DRIVER={SQL Server};SERVER=worldcuppitaco-db.mysql.database.azure.com;DATABASE=worldcuppitaco;UID=worldcuppitaco;PWD=Copadomundo2022")
+params = urllib.parse.quote_plus("DRIVER={ODBC Driver 17 for SQL Server};SERVER=worldcuppitaco-db.mysql.database.azure.com;DATABASE=worldcuppitaco;UID=worldcuppitaco;PWD=Copadomundo2022")
 
 
 app = Flask(__name__)
