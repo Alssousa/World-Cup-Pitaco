3869ec6dfc33eb946fceb9a9c67bea69a6ac2d3e
Alssousa <alssousa@proton.me>
2022-11-24 10:36:14 -0300

    Teste compatibiliade comparação datimes

--- copadomundo/routes.py
+++ copadomundo/routes.py
@@ -253,6 +253,7 @@ def palpite(usuario, partida, pitaco):
     
     fuso_horario = timezone('America/Sao_Paulo')
     data_atual = datetime.now().astimezone(fuso_horario)
+    partida.data_partida = partida.data_partida.astimezone(fuso_horario)
     print(f"\n{pitaco}\n")
     
     if partida.data_partida.astimezone(fuso_horario) >= data_atual:
@@ -268,7 +269,7 @@ def palpite(usuario, partida, pitaco):
             flash('Seu palpite foi realizado com sucesso.', 'alert-success')
             return redirect(url_for('home'))
     else:
-        flash('Não é possível realizar ações durante ou depois do evento.', 'alert-danger')
+        flash(f'Não é possível realizar ações durante ou depois do evento. Hora da partida:{partida.data_partida.astimezone(fuso_horario) } - Hora Sistema:{data_atual}', 'alert-danger')
         return redirect(url_for('home'))
     
     return render_template('home.html') 
