75daca75d635277cefb1d19c235bf150f3651660
Alssousa <alssousa@proton.me>
2022-11-24 09:51:02 -0300

    24/11

--- copadomundo/routes.py
+++ copadomundo/routes.py
@@ -254,8 +254,8 @@ def palpite(usuario, partida, pitaco):
     fuso_horario = timezone('America/Sao_Paulo')
     data_atual = datetime.now().astimezone(fuso_horario)
     print(f"\n{pitaco}\n")
-
-    if partida.data_partida >= data_atual.now():
+    
+    if partida.data_partida >= data_atual:
         #Tenho que verificar se o valor do palpite: casa, empate e fora e verificar
         if Partida.query.filter_by(id=partida.id).filter(Partida.palpites.any(id_usuario=usuario.id)).first() != None:
             print(Partida.query.filter(Partida.palpites.any(id_usuario=usuario.id)).first())
