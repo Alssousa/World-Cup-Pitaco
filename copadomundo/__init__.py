5b9bc4bb809e1414bdcbbbbbf74ab8b3a14fe3dc
Alssousa <alssousa@proton.me>
2022-12-02 20:01:13 -0300

    .config do db restaurado

--- copadomundo/__init__.py
+++ copadomundo/__init__.py
@@ -6,7 +6,7 @@ from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'C0P4_D0_MUND0_P1T4C0'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://alexandre:Al180202@localhost/worldcuppitaco'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://worldcuppitaco:Copadomundo2022@worldcuppitaco-db.mysql.database.azure.com:3306/worldcuppitaco?ssl_ca=DigiCertGlobalRootCA.crt.pem'
 app.jinja_env.add_extension('jinja2.ext.loopcontrols')
 
 database = SQLAlchemy(app)
